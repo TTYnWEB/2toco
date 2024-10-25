f9fdd3fc8d828d4d43840625d0260a0b6a43c60f
PseudoTat <tatthewyzard@outlook.com>
2024-10-24 23:03:00 -0500

    Routes component

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,37 +3,14 @@ import {
   Container,
   Stack,
 } from '@mui/material';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Switch,
-  Route,
-} from 'react-router-dom';
 // IMPs - local
-import Home     from './Home';
-import Redir    from './Redir';
-import NotFound from './NotFound';
+import Routes from './Routes';
 
 export default function App() {
   return (
     <Container>
       <Stack spacing={2} sx={{ alignItems: 'center' }}>
-        <Router>
-          <Switch>
-            <Route path="/:guid([a-z])">
-              <Redir />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/404">
-              <NotFound />
-            </Route>
-            <Route path="*">
-              <Redirect to="/404" />
-            </Route>
-          </Switch>
-        </Router>
+        <Routes />
       </Stack>
     </Container>
   );
