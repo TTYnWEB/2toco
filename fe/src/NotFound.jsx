2dcac978da7b0c4928dc853bda3c5aaf5e278c38
PseudoTat <tatthewyzard@outlook.com>
2024-10-28 15:17:33 -0500

    moving components to components dir

--- fe/src/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <>
-      <h1>404</h1>
-      <h3>File Not Found</h3>
-      <h2><a href="http://localhost:3000">2to.co</a></h2>
-    </>
-  );
-}
